105a9a89d26c5ef0ff4c0a809073ca2a658d9d11
ben <ben>
2020-02-21 01:13:21 +0530

    CRD finished

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
+// Require file-based-data-storage module
 const FileBasedDataStorage = require('./file-based-data-storage');
 
-let fbds = new FileBasedDataStorage();
 
-try {
-    fbds.create('profile2', {name: 'benny', job: 'IT'}, 300);
-} catch(ex) {
-    console.error(ex);
+// Initialize class with optional filePath parameter
+let fbds = new FileBasedDataStorage('');
+
+
+// Create a key value pair. Last parameter is timeToLive which is optional
+let response_create = fbds.create('profile2', {name: 'benny1', job: 'IT'}, 30);
+// if response has error then print the error, otherwise print success message
+if (response_create.err) {
+    console.error(response_create.err);
+} else {
+    console.log('Value created successfully');
+}
+
+
+// Read a value by key
+let response_read = fbds.read('profile2');
+if (response_read.err) {
+    console.error(response_read.err);
+} else {
+    console.log('Value => ', response_read.value);
+}
+
+
+// Delete a value by key
+let response_delete = fbds.delete('profile2');
+if (response_delete.err) {
+    console.error(response_delete.err);
+} else {
+    console.log('Value deleted successfully');
 }
--- file-based-data-storage.js
+++ file-based-data-storage.js
@@ -5,6 +5,8 @@ class FileBasedDataStorage {
 
     constructor(filePath = 'data/storage.json') {
 
+        console.log(path.basename(filePath));
+
         this._filePath = filePath;
     }
 
@@ -12,17 +14,17 @@ class FileBasedDataStorage {
 
         // Check key is valid
         if (!key || key.toString().length > 32) {
-            throw 'Not a valid key';
+            return { err: 'Not a valid key' };
         }
 
         // Check value is valid
         if (!value || typeof value !== 'object' || Array.isArray(value) || Buffer.byteLength(JSON.stringify(value)) > 1024 * 16) {
-            throw 'Not a valid value';
+            return { err: 'Not a valid value' };
         }
 
         // Check timetoLive is a number
-        if (isNaN(timeToLive) || timeToLive <= 0 || timeToLive % 1 !== 0) {
-            throw 'Not a valid timeToLive';
+        if (timeToLive != 0 && (isNaN(timeToLive) || timeToLive % 1 !== 0)) {
+            return { err: 'Not a valid timeToLive' };
         }
 
         // Check existing file size is exceed 1 GB
@@ -30,9 +32,9 @@ class FileBasedDataStorage {
             
             let fileSize = fs.statSync(this._filePath).size;
             if (fileSize > 1024 * 1024 * 1024) {
-                throw 'File size is already reached the limit';
+                return { err: 'File size is already reached the limit' };
             } else if (fileSize + Buffer.byteLength(JSON.stringify(value)) > 1024 * 1024 * 1024) {
-                throw 'File size will be exceed the limit';
+                return { err: 'File size will be exceed the limit' };
             }
         } else {
             // Make directory
@@ -46,14 +48,59 @@ class FileBasedDataStorage {
         // Check key is alread exist
         let storageData = JSON.parse(fs.readFileSync(this._filePath));
         if (storageData.hasOwnProperty(key)) {
-            throw 'Key is already exist';
+            return { err: 'Key is already exist' };
         }
 
         // Create a new key value pair to file
-        storageData[key] = {value, timeToLive: timeToLive != 0 ? (new Date().getTime() + timeToLive * 1000) : false};
+        storageData[key] = {value, timeToLive: timeToLive != 0 ? (new Date()).getTime() + timeToLive * 1000 : false};
 
         // Write object into the file
         fs.writeFileSync(this._filePath, JSON.stringify(storageData));
+
+        return { success: true };
+    }
+
+    read(key) {
+
+        // Check file is exists
+        if (!fs.existsSync(this._filePath)) {
+            return { err: 'File is not created' };
+        }
+
+        // Read file data
+        let storageData = JSON.parse(fs.readFileSync(this._filePath));
+
+        // Check key is exist and timeToLive
+        if (!storageData.hasOwnProperty(key) || (storageData[key].timeToLive !== false && storageData[key].timeToLive < (new Date()).getTime())) {
+            return { err: 'Key is not exist' };
+        }
+
+        // Return the value of the key
+        return { value: storageData[key].value };
+    }
+
+    delete(key) {
+
+        // Check file is exists
+        if (!fs.existsSync(this._filePath)) {
+            return { err: 'File is not created' };
+        }
+
+        // Read the file data
+        let storageData = JSON.parse(fs.readFileSync(this._filePath));
+
+        // Check key is exist and timeToLive
+        if (!storageData.hasOwnProperty(key) || (storageData[key].timeToLive !== false && storageData[key].timeToLive < (new Date()).getTime())) {
+            return { err: 'Key is not exist' };
+        }
+
+        // delete the from storageData
+        delete storageData[key];
+
+        // store the data to the same file
+        fs.writeFileSync(this._filePath, JSON.stringify(storageData));
+
+        return { success: true };
     }
 }
 
