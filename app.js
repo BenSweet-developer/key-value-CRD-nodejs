83c670a71411611a47896faaadd127bb45e43c45
BenSweet-developer <33755593+BenSweet-developer@users.noreply.github.com>
2020-02-28 01:24:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,38 +2,47 @@
 const FileBasedDataStorage = require('./file-based-data-storage');
 
 
-// Initialize class with optional filePath parameter
-let fbds = new FileBasedDataStorage();
+try {
+
+    // Initialize class with optional filePath
+    let fbds = new FileBasedDataStorage('./data/storage-data.json');
 
 
-// Create a key value pair. Last parameter is timeToLive which is optional
-let responseCreate = fbds.create('profile', {name: 'Benny Sweetson M', job: 'IT'}, 30);
 
-// if response has error then print the error, otherwise print message.
-if (responseCreate.err) {
-    console.error(responseCreate.err);
-} else {
-    console.log(responseCreate.message);
-}
+    // Create a key value pair. Last parameter is timeToLive which is optional
+    let responseCreate = fbds.create('profile', {name: 'Benny Sweetson M', job: 'IT'}, 30);
 
-
-// Read a value by key
-let responseRead = fbds.read('profile');
-
-// if response has error then print the error, otherwise print message.
-if (responseRead.err) {
-    console.error(responseRead.err);
-} else {
-    console.log('Value => ', responseRead.value);
-}
+    // if response has error then print the error, otherwise print message.
+    if (responseCreate.err) {
+        console.error(responseCreate.err);
+    } else {
+        console.log(responseCreate.message);
+    }
 
 
-// Delete a value by key
-let responseDelete = fbds.delete('profile');
 
-// if response has error then print the error, otherwise print message.
-if (responseDelete.err) {
-    console.error(responseDelete.err);
-} else {
-    console.log(responseDelete.message);
+    // Read a value by key
+    let responseRead = fbds.read('profile');
+
+    // if response has error then print the error, otherwise print message.
+    if (responseRead.err) {
+        console.error(responseRead.err);
+    } else {
+        console.log('Value => ', responseRead.value);
+    }
+
+
+    
+    // Delete a value by key
+    let responseDelete = fbds.delete('profile');
+
+    // if response has error then print the error, otherwise print message.
+    if (responseDelete.err) {
+        console.error(responseDelete.err);
+    } else {
+        console.log(responseDelete.message);
+    }
+
+} catch (e) {
+    console.error(e.name + ': ' + e.message)
 }
